33631d1aa7010c2d411f7e23921e00c3a8d55b9f
songsongsong97 <44999319+songsongsong97@users.noreply.github.com>
2024-11-28 12:04:45 +0800

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,6 +5,42 @@ import { container, item } from "../animations/animation";
 export default function Experience() {
   const experiences = [
     {
+      title: "Software Developer",
+      description:
+        "Developed a merchant monitoring system integrated with natural language processing microservices to \
+        identify early indicators of high-risk merchant activity and potential fraud.",
+      company: "ADVANCE.AI - ADVANCE.BizQ",
+      from: "Nov 2023",
+      until: "Present",
+      skills: [
+        "Django",
+        "ReactJS",
+        "PostgreSQL",
+        "Docker",
+        "Kakfa",
+        "Nginx",
+        "Linux",
+        "AWS",
+      ],
+    },
+    {
+      title: "Software Engineer (Python)",
+      description:
+        "Built API services and tools to provide DNS hosting and certificate manager services for over thousands of domains.",
+      company: "Merquri Pte Ltd.",
+      from: "Jul 2021",
+      until: "Present",
+      skills: [
+        "Flask",
+        "ReactJS",
+        "MySQL",
+        "Docker",
+        "Kubernetes",
+        "Gitlab CI",
+        "AWS",
+      ],
+    },
+    {
       title: "Software Engineer (Python)",
       description:
         "Built API services and tools to provide DNS hosting and certificate manager services for over thousands of domains.",
