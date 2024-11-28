1c79ae7e999f77e71d93b0ce552899046c59aa33
songsongsong97 <44999319+songsongsong97@users.noreply.github.com>
2024-11-28 12:05:16 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="text-center mx-auto">
       <p className="font-sans mb-8 text-black dark:text-white">
-        © 2023 Song Lee Yee. All rights reserved.
+        {`© ${currentYear} Song Lee Yee. All rights reserved.`}
       </p>
     </div>
   );
